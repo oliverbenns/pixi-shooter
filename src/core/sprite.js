24f8bc5c083af0585ffce7b8b178de7c736d8c4b
Oliver Benns <oliverbenns@gmail.com>
2017-08-29 23:31:03 +0100

    Fix comment

--- src/core/sprite.js
+++ src/core/sprite.js
@@ -1,6 +1,6 @@
 import * as Pixi from 'pixi.js';
 
-// @NOTe: This is doing nothing right now. Maybe I have compose this instead. eg. class GameObject { sprite: Pixi.Sprite }
+// @NOTE: This is doing nothing right now. Maybe I compose this instead. eg. class GameObject { sprite: Pixi.Sprite }
 export default class Sprite extends Pixi.Sprite {
   constructor(texture) {
     super(texture);
